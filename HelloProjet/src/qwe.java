85e20d0138116e541ef7bcbc66985ab34e8e2eec
532532032 <54729969+532532032@users.noreply.github.com>
2020-04-11 15:44:05 +0800

    增加功能

--- HelloProjet/src/qwe.java
+++ HelloProjet/src/qwe.java
@@ -7,5 +7,9 @@ public class qwe {
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
+        System.out.println("HelloWorld!");
+        System.out.println("HelloWorld!");
+        System.out.println("HelloWorld!");
+        System.out.println("HelloWorld!");
     }
 }
